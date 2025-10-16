d46a4b95cc24ba42418e00f4f25ca64e178f4e18
Alief Irfan <giftforoscar@gmail.com>
2025-10-16 14:13:52 +0800

    Repair Task 3 and 4

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const drivers = [
 
 // Task 2: JSON Data Operations
 // TODO : show all the drivers name in the console
-console.log("Drivers Names:");
+console.log("\nDrivers Names:\n");
 drivers.forEach(driver=> console.log(driver.name));
 
 // TODO: add additional driver to drivers array
@@ -28,6 +28,12 @@ drivers.push({
       vehicleType: "MPV",
       isAvailable: true,
       rating: 4.2
+},
+{
+      name: "Firdaus Ibrahim",
+      vehicleType: "Hatchback",
+      isAvailable: false,
+      rating: 4.6
 });
 
 // show the data in console
@@ -40,30 +46,39 @@ async function main() {
   const client = new MongoClient(uri);
 
   try {
-    await client.connect();   
+    await client.connect();
+    console.log("\nConnected to MongoDB !");
+
     const db = client.db("testDB");
+    const users = db.collection("users");
 
     // Task 3: Insert Drivers into MongoDB
-    const driversCollection = db.collection("drivers");
+    const insertResult = await users.insertMany(drivers);
+    console.log(`Inserted ${insertResult.insertedCount} drivers into MongoDB.`); 
+    await users.deleteMany({});
+    await users.insertMany(drivers);
 
-    drivers.forEach(async (driver) => {
-      const result = await driversCollection.insertOne(driver);
-      console.log('New driver created with result: ${result}');
-    });
 
     // Task 4: Query and Updates Drivers
-    const availableDrivers = await db.collection('drivers').find({
-      isAvailable: true,
-      rating: { $gte: 4.5 }
-    }).toArray();
-    console.log("Available drivers:", availableDrivers);
+    const availableDrivers = await users.find({ isAvailable: true, rating: { $gte: 4.5 } }).toArray();
+    console.log("\nAvailable drivers with rating ≥ 4.5: ",availableDrivers);
+    //console.log(availableDrivers);
 
-  } finally {
-    await client.close();
-  }
+    // Task 5: Update John Doe's rating +0.1
+    //const updateResult = await users.updateOne(
+      //{ name: "John Doe"},
+      //{ $inc: { rating: 0.10 }}
+    );
+    //console.log(`\nUpdated ${updateResult.modifiedCount} driver (John Doe).`);
 
-    const result = await users.insertOne({ name: "Test User", age: 25 });
-    console.log("Inserted document with _id:", result.insertedId);
+    //const updatedJohn = await users.findOne({ name: "John Doe" });
+    //console.log(`John Doe's new rating: ${updatedJohn.rating}`);
+
+    //console.log(updateResult);
+
+    
+
+
   } catch (err) {
     console.error("Error:", err);
   } finally {

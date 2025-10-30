5b357cf9c8e3975797e2d38f4980b453ef9608f4
Alief Irfan <giftforoscar@gmail.com>
2025-10-30 12:21:27 +0800

    W3 : Step 2 Create Ride Endpoints - PATCH, DELETE

--- index.js
+++ index.js
@@ -128,3 +128,39 @@ app.post('/rides', async (req, res) => {
   }
 });
 
+// WEEK 3 PATCH /rides/:id - Update ride status
+app.patch('/rides/:id', async (req, res) => {
+  try {
+    const result = await db.collection('rides').updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: { status: req.body.status } }
+    );
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({ error: "Ride not found" });
+    }
+    res.status(200).json({ updated: result.modifiedCount });
+  
+  } catch (err) {
+    //Handle invalid ID format or DB errors
+    res.status(400).json({ error: "Invalid ride ID or data" });
+  }
+});
+
+// DELETE /rides/:id - Cancel a ride
+app.delete('/rides/:id', async (req, res) => {
+  try {
+    const result = await db.collection('rides').deleteOne(
+      { _id: new ObjectId(req.params.id) }
+    );
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: " Ride not found" });
+    }
+    res.status(200).json({ deleted: result.deleteCount });
+  
+  } catch (err) {
+    res.status(400).json({ error: "Invalid ride ID"});
+  }
+});
+
